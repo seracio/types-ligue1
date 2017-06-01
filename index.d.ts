1ab813b5f3ff885c1036b183d3421b7f0496cad2
nmondon <nmondon@lefigaro.fr>
2017-06-01 13:08:10 +0200

    update types

--- index.d.ts
+++ index.d.ts
@@ -1,111 +1,117 @@
 declare namespace Ligue1 {
+
+    // a time on a match
+    interface Time {
+        time: number
+        extra?: number
+    }
+
+    // a team stat is a particular stat for a given 
+    // team on a givem match
     interface TeamStat {
-        label: string,
-        value: string,
-        matchId: string,
-        teamId: string,
-        week: number,
+        label: string
+        value: string
+        matchId: string
+        teamId: string
+        week: number
         season: string
     }
 
+    // a player stat is a particular stat for a given
+    // player on a given match
     interface PlayerStat {
-        label: string,
-        value: string,
-        matchId: string,
-        playerId: string,
-        teamId: string,
-        week: number,
+        label: string
+        value: string
+        matchId: string
+        playerId: string
+        teamId: string
+        week: number
         season: string
+    }
+
+    // a player participation on a given match
+    interface Participation {
+        matchId: string
+        playerId: string
+        teamId: string
+        in: Time
+        out: Time
+        isSubstitute: boolean
+        isCaptain: boolean
     }
 
     interface RawMatch {
-        id: string,
-        homeId: string,
-        awayId: string,
-        homeScore: number,
-        awayScore: number,
-        homeCoaches: Array<string>,
-        awayCoaches: Array<string>,
-        referee?: string,
-        week: number,
-        season: string,
-        affluence?: number,
-        temperature?: number,
-        homeTeam: Array<string>,
-        awayTeam: Array<string>,
-        homeSubsitutes: Array<string>,
-        awaySubstitutes: Array<string>
+        id: string
+        homeId: string
+        awayId: string
+        homeScore: number
+        awayScore: number
+        homeCoaches: Array<string>
+        awayCoaches: Array<string>
+        referee: string
+        week: number
+        season: string
+        affluence: number
+        temperature: number
     }
 
     interface Match extends RawMatch {
-        date: Date,
+        date: Date
     }
 
     interface NetworkMatch extends RawMatch {
-        date: string,
+        date: string
     }
 
     interface Team {
-        id: string,
-        label: string,
-        logo?: string
+        id: string
+        label: string
+        logo: string
     }
 
     interface Goal {
-        matchId: string,
-        playerId: string,
-        playerAssistId?: string,
-        isPenalty: boolean,
-        isOG: boolean,
-        time: number,
-        extra?: number,
-        index: number,
-        week: number,
+        matchId: string
+        playerId: string
+        playerAssistId?: string
+        isPenalty: boolean
+        isOG: boolean
+        time: Time
+        index: number
+        week: number
         season: string
     }
 
     interface Card {
-        type: 'red' | 'yellow',
-        time: number,
-        index: number,
-        extra?: number,
-        matchId: string,
-        playerId: string,
-        week: number,
+        type: 'red' | 'yellow'
+        time: Time,
+        index: number
+        matchId: string
+        playerId: string
+        week: number
         season: string
     }
 
     interface RawPlayer {
-        id: string,
-        name: string,
-        nationality: String,
+        id: string
+        name: string
+        nationality: String
     }
 
     interface Player extends RawPlayer {
-        birthday: Date,
+        birthday: Date
     }
 
     interface NetworkPlayer extends RawPlayer {
-        birthday: string,
+        birthday: string
     }
 
     interface Odd {
-        matchId: string,
-        v: number,
-        n: number,
+        matchId: string
+        v: number
+        n: number
         d: number
     }
 
-    interface Substitution {
-        matchId: string,
-        playerInId: string,
-        playerOutId: string,
-        teamId: string,
-        time: number,
-        extra: number,
-        week: number,
-        season: string
-    }
 }
 
 export = Ligue1
